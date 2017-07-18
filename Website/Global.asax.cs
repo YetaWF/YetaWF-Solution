0cd9497034368b894f6edaec41268b21b5a3895e
YetaWF <mikevdm@YetaWF.com>
2017-07-18 11:58:54 -0400

    Initial commit

---
+++ Website/Global.asax.cs
@@ -0,0 +1,175 @@
+﻿/* Copyright © 2017 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
+
+using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Web;
+using YetaWF.Core;
+using YetaWF.Core.Extensions;
+using YetaWF.Core.Log;
+using YetaWF.Core.Site;
+using YetaWF.Core.Support;
+
+#if DEBUG
+#endif
+
+namespace YetaWF {
+    public class MvcApplication : System.Web.HttpApplication {
+
+        private const string AppSettingsFile = "Appsettings.json";
+
+        public MvcApplication() {
+            AcquireRequestState += new EventHandler(MvcApplication_AcquireRequestState);
+            Error += MvcApplication_Error;
+
+            YetaWFManager.RootFolder = YetaWFManager.UrlToPhysical("/").TrimEnd(new char[] { '\\' });
+            WebConfigHelper.Init(Path.Combine(YetaWFManager.RootFolder, Globals.DataFolder, AppSettingsFile));
+
+            // retrieve the default domain to verify it has been defined in Appsettings.json
+            string siteDomain = YetaWFManager.DefaultSiteName;
+            // We're renaming the __RequestVerificationToken cookie (not the <input hidden> name) for anti-forgery.
+            // We ran into this problem where we host https://yetawf.com in a YetaWF instance (one IIS site) and host
+            // http://forum.yetawf.com (another IIS site elsewhere). Because they are the same domain name, just different
+            // subdomains, browsers will return TWO __RequestVerificationToken cookies (when the user has visited both
+            // sites) causing anti-forgery confusion. This can also be fixed with machinekey settings (forcing both sites
+            // to use the same keys). But it is easier to just rename our COOKIE. While both sites will still get
+            // both cookies, they now use different names, eliminating the confusion.
+            // Please note the distinction between the __RequestVerificationToken COOKIE and the
+            // __RequestVerificationToken hidden input field. Only the cookie is renamed. There is no need to
+            // rename the input field.
+            System.Web.Helpers.AntiForgeryConfig.CookieName = "__ReqVerToken_" + siteDomain;
+        }
+
+        void MvcApplication_Error(object sender, EventArgs e) {
+            // flush log on error, but avoid log spamming
+            if (LastError == null || DateTime.Now > ((DateTime)LastError).AddSeconds(10)) {
+                Logging.ForceFlush();// make sure this is recorded immediately so we can see it in the log
+                LastError = DateTime.Now;
+            }
+        }
+        private static DateTime? LastError = null;// use local time
+
+        void Session_Start(object sender, EventArgs e) {
+            // Code that runs when a new session is started
+
+            // Ensure SessionID in order to prevent the following exception
+            // when the Application Pool Recycles
+            // [HttpException]: Session state has created a session id, but cannot
+            // 				   save it because the response was already flushed ...
+            string sessionId = Session.SessionID;
+        }
+
+        public void MvcApplication_AcquireRequestState(object sender, EventArgs e) {
+
+            HttpRequest httpReq = HttpContext.Current.Request;
+            Uri uri = httpReq.Url;
+
+            // Url rewrite can cause "Cannot use a leading .. to exit above the top directory."
+            //http://stackoverflow.com/questions/3826299/asp-net-mvc-urlhelper-generateurl-exception-cannot-use-a-leading-to-exit-ab
+            httpReq.ServerVariables.Remove("IIS_WasUrlRewritten");
+
+            // Determine which Site folder to use based on URL provided
+            bool forcedHost;
+            bool newSwitch;
+            string host = YetaWFManager.GetRequestedDomain(uri, httpReq.QueryString[Globals.Link_ForceSite], out forcedHost, out newSwitch);
+            string host2 = null;
+
+            // check if such a site definition exists (accounting for www. or other subdomain)
+            string[] domParts = host.Split(new char[] { '.' });
+            if (domParts.Length > 2) {
+                if (domParts.Length > 3 || domParts[0] != "www")
+                    host2 = host;
+                host = string.Join(".", domParts, domParts.Length - 2, 2);// get just domain as a fallback
+            }
+            SiteDefinition site = null;
+            if (!string.IsNullOrWhiteSpace(host2)) {
+                site = SiteDefinition.LoadSiteDefinition(host2);
+                if (site != null)
+                    host = host2;
+            }
+            if (site == null) {
+                site = SiteDefinition.LoadSiteDefinition(host);
+                if (site == null) {
+                    if (forcedHost) { // non-existent site requested
+                        HttpContext.Current.Response.Status = Logging.AddErrorLog("404 Not Found");
+                        HttpContext.Current.ApplicationInstance.CompleteRequest();
+                        return;
+                    }
+                    site = SiteDefinition.LoadSiteDefinition(null);
+                    if (site == null) {
+                        if (SiteDefinition.INITIAL_INSTALL) {
+                            // use a skeleton site for initial install
+                            // this will be updated when the model is installed
+                            site = new SiteDefinition {
+                                Identity = SiteDefinition.SiteIdentitySeed,
+                                SiteDomain = host,
+                            };
+                        } else {
+                            throw new InternalError("Couldn't obtain a SiteDefinition object");
+                        }
+                    }
+                }
+            }
+            // We have a valid request for a known domain or the default domain
+            // create a YetaWFManager object to keep track of everything (it serves
+            // as a global anchor for everything we need to know while processing this request)
+            YetaWFManager manager = YetaWFManager.MakeInstance(host);
+            // Site properties are ONLY valid AFTER this call to YetaWFManager.MakeInstance
+
+            manager.CurrentSite = site;
+
+            manager.HostUsed = uri.Host;
+            manager.HostPortUsed = uri.Port;
+            manager.HostSchemeUsed = uri.Scheme;
+
+            if (forcedHost && newSwitch) {
+                if (!manager.HasSuperUserRole) { // if superuser, don't log off (we could be creating a new site)
+                    // A somewhat naive way to log a user off, but it's working well and also handles 3rd party logins correctly.
+                    // Since this is only used during site development, it's not critical
+                    string logoffUrl = WebConfigHelper.GetValue<string>("MvcApplication", "LogoffUrl", null);
+                    if (string.IsNullOrWhiteSpace(logoffUrl))
+                        throw new InternalError("MvcApplication LogoffUrl not defined in web.cofig/appsettings.json - this is required to switch between sites so we can log off the site-specific currently logged in user");
+                    Uri newUri;
+                    if (uri.IsLoopback) {
+                        // add where we need to go next (w/o the forced domain, we're already on this domain (localhost))
+                        newUri = RemoveQsKeyFromUri(uri, Globals.Link_ForceSite);
+                    } else {
+                        newUri = new Uri("http://" + host);// new site to display
+                    }
+                    logoffUrl += YetaWFManager.UrlEncodeArgs(newUri.ToString());
+                    logoffUrl += (logoffUrl.Contains("?") ? "&" : "?") + "ResetForcedDomain=false";
+                    HttpContext.Current.Response.Status = Logging.AddLog("302 Found - {0}", logoffUrl).Truncate(100);
+                    HttpContext.Current.Response.AddHeader("Location", manager.CurrentSite.MakeUrl(logoffUrl));
+                    HttpContext.Current.ApplicationInstance.CompleteRequest();
+                    return;
+                }
+            }
+
+            // Make sure we're using the "official" URL, otherwise redirect 301
+            if (site.EnforceSiteUrl) {
+                if (uri.IsAbsoluteUri) {
+                    if (!manager.IsLocalHost && !forcedHost && string.Compare(manager.HostUsed, site.SiteDomain, true) != 0) {
+                        UriBuilder newUrl = new UriBuilder(uri);
+                        newUrl.Host = site.SiteDomain;
+                        HttpContext.Current.Response.Status = Logging.AddLog("301 Moved Permanently - {0}", newUrl.ToString()).Truncate(100);
+                        HttpContext.Current.Response.AddHeader("Location", newUrl.ToString());
+                        HttpContext.Current.ApplicationInstance.CompleteRequest();
+                        return;
+                    }
+                }
+            }
+            // IE rejects our querystrings that have encoded "?" (%3D) even though that's completely valid
+            // so we have to turn of XSS protection (which is not necessary in YetaWF anyway)
+            HttpContext.Current.Response.Headers.Add("X-Xss-Protection", "0");
+        }
+
+        private Uri RemoveQsKeyFromUri(Uri uri, string qsKey) {
+            UriBuilder newUri = new UriBuilder(uri);
+            NameValueCollection qs = System.Web.HttpUtility.ParseQueryString(newUri.Query);
+            qs.Remove(qsKey);
+            newUri.Query = qs.ToString();
+            return newUri.Uri;
+        }
+    }
+}
+
---
+++ Website/Startup5.cs
@@ -0,0 +1,51 @@
+﻿/* Copyright © 2017 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
+
+using System.Web.Mvc;
+using System.Web.Routing;
+using YetaWF.Core.Log;
+using YetaWF.Core.Packages;
+using YetaWF.Core.Site;
+using YetaWF.Core.Support;
+
+[assembly: WebActivatorEx.PostApplicationStartMethod(typeof(YetaWF.App_Start.Startup), "Start")]
+
+namespace YetaWF.App_Start {
+    public class Startup {
+
+        public static void Start() {
+
+            YetaWFManager manager = YetaWFManager.MakeInstance("__STARTUP"); // while loading packages we need a manager
+            manager.CurrentSite = new SiteDefinition();
+
+            Logging.SetupLogging();
+            Logging.AddLog("YetaWF.App_Start.Startup starting");
+
+            RouteTable.Routes.IgnoreRoute("File.image/{*pathInfo}");
+            RouteTable.Routes.IgnoreRoute("FileHndlr.image/{*pathInfo}");
+
+            Logging.AddLog("Calling AreaRegistration.RegisterAllAreas()");
+            AreaRegistration.RegisterAllAreas();
+            Logging.AddLog("Adding filters");
+            //GlobalFilters.Filters.Add(new HandleErrorAttribute());
+
+            Logging.AddLog("Adding catchall route");
+            RouteTable.Routes.MapRoute(
+                "Page",
+                "{*__path}",
+                new { controller = "Page", action = "Show" },
+                new string[] { "YetaWF.Core.Controllers", } // namespace
+            );
+
+            // Call all classes that expose the interface IInitializeApplicationStartup
+            YetaWF.Core.Support.Startup.CallStartupClasses();
+
+            // Find all the views/areas that are available to the website (i.e., core + modules)
+            ViewEnginesStartup.Start();
+
+            Package.UpgradeToNewPackages();
+
+            YetaWF.Core.Support.Startup.Started = true;
+            Logging.AddLog("YetaWF.App_Start.Startup completed");
+        }
+    }
+}
