0a8656170f9fc7395df91f64dee2d20e81141663
YetaWF <mikevdm@YetaWF.com>
2017-08-12 13:35:19 -0400

    don't add gulp

--- post-clone.bat
+++ post-clone.bat
@@ -1,7 +1,6 @@
 Utilities\ProjectSettings\bin\Softelvdm.ProjectSettings.exe SetMVC5 Symlinks
 cd Website
 cmd /c npm install
-cmd /c npm install -g gulp
 cmd /c npm rebuild node-sass
 cmd /c bower install
 cd ..
